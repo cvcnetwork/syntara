8c4f2e521d9be45d36b152f854ac29d55a9aa6cc
cvctimlindsay <timlindsay@cvc.tv>
2014-07-07 09:44:03 +1000

    add timezone field on user edit

--- src/views/user/show-user.blade.php
+++ src/views/user/show-user.blade.php
@@ -34,6 +34,10 @@
                                 <label class="control-label">{{ trans('syntara::users.first-name') }}</label>
                                 <input class="col-lg-12 form-control" type="text" id="first_name" name="first_name" value="{{ $user->first_name }}">
                             </div>
+                            <div class="form-group">
+                                <label class="control-label">Timezone</label>
+                                <select class="col-lg-12 form-control" type="text" id="timezone" name="timezone" value="<?php echo getTzOptions($user->timezone) ?>"></select>
+                            </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="form-group">
@@ -94,4 +98,58 @@
         </div>
     </div>
 </div>
+<?php
+
+function getTzOptions($selectedzone)
+{
+  $all = timezone_identifiers_list();
+
+  if (empty($selectedzone)) {
+    $selectedzone = 'UTC';
+  }
+
+  $i = 0;
+  foreach ($all AS $zone) {
+    $zone = explode('/', $zone);
+    $zonen[$i]['continent'] = isset($zone[0]) ? $zone[0] : '';
+    $zonen[$i]['city'] = isset($zone[1]) ? $zone[1] : '';
+    $zonen[$i]['subcity'] = isset($zone[2]) ? $zone[2] : '';
+    $i++;
+  }
+
+  asort($zonen);
+
+  $structure = '';
+
+  foreach ($zonen AS $zone) {
+    extract($zone);
+    if ($continent == 'Africa' || $continent == 'America' || $continent == 'Antarctica' || $continent == 'Arctic' || $continent == 'Asia' || $continent == 'Atlantic' || $continent == 'Australia' || $continent == 'Europe' || $continent == 'Indian' || $continent == 'Pacific') {
+      if (!isset($selectcontinent)) {
+        $structure .= '<optgroup label="' . $continent . '">'; // continent
+      }
+      elseif ($selectcontinent != $continent) {
+        $structure .= '</optgroup><optgroup label="' . $continent . '">'; // continent
+      }
+
+      if (isset($city) != '') {
+        if (!empty($subcity) != '') {
+          $city = $city . '/' . $subcity;
+        }
+        $structure .= "<option " . ((($continent . '/' . $city) == $selectedzone) ? 'selected="selected "' : '') . " value=\"" . ($continent . '/' . $city) . "\">" . str_replace('_', ' ', $city) . "</option>"; //Timezone
+      }
+      else {
+        if (!empty($subcity) != '') {
+          $city = $city . '/' . $subcity;
+        }
+        $structure .= "<option " . (($continent == $selectedzone) ? 'selected="selected "' : '') . " value=\"" . $continent . "\">" . $continent . "</option>"; //Timezone
+      }
+
+      $selectcontinent = $continent;
+    }
+  }
+  $structure .= '</optgroup>';
+  return $structure;
+}
+
+?>
 @stop
