871ee576ff997688052db25ea6f9ec4616659fb1
cvctimlindsay <timlindsay@cvc.tv>
2014-07-07 09:43:01 +1000

    Add function to generate timezone select box for new users

--- src/views/user/new-user.blade.php
+++ src/views/user/new-user.blade.php
@@ -69,4 +69,58 @@
         </div>
     </div>
 </div>
+<?php
+
+function getTzOptions($selectedzone)
+{
+  $all = timezone_identifiers_list();
+
+  if (empty($selectedzone)) {
+    $selectedzone = 'UTC';
+  }
+
+  $i = 0;
+  foreach ($all AS $zone) {
+    $zone = explode('/', $zone);
+    $zonen[$i]['continent'] = isset($zone[0]) ? $zone[0] : '';
+    $zonen[$i]['city'] = isset($zone[1]) ? $zone[1] : '';
+    $zonen[$i]['subcity'] = isset($zone[2]) ? $zone[2] : '';
+    $i++;
+  }
+
+  asort($zonen);
+
+  $structure = '';
+
+  foreach ($zonen AS $zone) {
+    extract($zone);
+    if ($continent == 'Africa' || $continent == 'America' || $continent == 'Antarctica' || $continent == 'Arctic' || $continent == 'Asia' || $continent == 'Atlantic' || $continent == 'Australia' || $continent == 'Europe' || $continent == 'Indian' || $continent == 'Pacific') {
+      if (!isset($selectcontinent)) {
+        $structure .= '<optgroup label="' . $continent . '">'; // continent
+      }
+      elseif ($selectcontinent != $continent) {
+        $structure .= '</optgroup><optgroup label="' . $continent . '">'; // continent
+      }
+
+      if (isset($city) != '') {
+        if (!empty($subcity) != '') {
+          $city = $city . '/' . $subcity;
+        }
+        $structure .= "<option " . ((($continent . '/' . $city) == $selectedzone) ? 'selected="selected "' : '') . " value=\"" . ($continent . '/' . $city) . "\">" . str_replace('_', ' ', $city) . "</option>"; //Timezone
+      }
+      else {
+        if (!empty($subcity) != '') {
+          $city = $city . '/' . $subcity;
+        }
+        $structure .= "<option " . (($continent == $selectedzone) ? 'selected="selected "' : '') . " value=\"" . $continent . "\">" . $continent . "</option>"; //Timezone
+      }
+
+      $selectcontinent = $continent;
+    }
+  }
+  $structure .= '</optgroup>';
+  return $structure;
+}
+
+?>
 @stop
